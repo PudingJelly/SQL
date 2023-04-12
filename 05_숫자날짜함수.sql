79ff9d2de50c93274c79c8e30352ce2475aaed97
Changju <changju7857@gmail.com>
2023-04-12 14:50:35 +0900

    2023.04.12

--- 05_숫자날짜함수.sql
+++ 05_숫자날짜함수.sql
@@ -18,3 +18,52 @@ FROM dual;
 SELECT
     ABS(-34)
 FROM dual;
+
+-- CEIL(올림), FLOOR(내림)
+SELECT CEIL(3.14), FLOOR(3.14)
+FROM dual;
+
+-- MOD(나머지)
+SELECT 10 / 4, MOD(10, 4)
+FROM dual;
+
+-- 날짜 함수
+SELECT sysdate FROM dual;
+SELECT systimestamp FROM dual;
+
+-- 날짜도 연산이 가능함
+SELECT sysdate + 1 FROM dual;
+
+SELECT first_name, sysdate - hire_date
+FROM employees; -- 일수
+
+SELECT first_name, hire_date, 
+(sysdate - hire_date) / 7 AS week
+FROM employees; -- 주수
+
+SELECT first_name, hire_date, 
+(sysdate - hire_date) / 365 AS year
+FROM employees; -- 년수
+
+-- 날짜 반올림, 절사
+SELECT ROUND(sysdate) FROM dual; 
+SELECT ROUND(sysdate, 'year') FROM dual; -- 년 기준으로 반올림
+SELECT ROUND(sysdate, 'month') FROM dual; -- 월 기준으로 반올림
+SELECT ROUND(sysdate, 'day') FROM dual; --1주일을 기준으로 반이 지났는지 안지났는지 확인
+-- 일 기준으로 반올림 (해당주의 일요일 날짜)
+
+SELECT TRUNC(sysdate) FROM dual; 
+SELECT TRUNC(sysdate, 'year') FROM dual; -- 년 기준으로 절사
+SELECT TRUNC(sysdate, 'month') FROM dual; -- 월 기준으로 절사
+SELECT TRUNC(sysdate, 'day') FROM dual; -- 일 기준으로 절사
+
+
+
+
+
+
+
+
+
+
+
