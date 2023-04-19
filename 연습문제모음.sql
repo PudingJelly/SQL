40d77735b50fcfa46628495c6ffd9f52ecced747
ChangjuMun <changju7857@gmail.com>
2023-04-19 15:49:23 +0900

    2023.04.19 MERGE

--- 연습문제모음.sql
+++ 연습문제모음.sql
@@ -17,8 +17,15 @@ FROM employees
 WHERE department_id = 50;
 
 --4. 50번 부서 사원의 이름, 부서번호, 직무아이디를 출력하세요.
+SELECT
+    first_name, department_id, job_id
+FROM employees
+WHERE department_id = 50;
 
 --5. 모든 사원의 이름, 급여 그리고 300달러 인상된 급여를 출력하세요.
+SELECT
+    first_name, salary, salary+300
+FROM employees;
 
 --6. 급여가 10000보다 큰 사원의 이름과 급여를 출력하세요.
 
